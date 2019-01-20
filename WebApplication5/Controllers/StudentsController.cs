830afacf599b6e10015226e7ab69746025e0883f
nzaqaryan1999 <nzaqaryan1999@gmail.com>
2019-01-20 18:21:09 +0400

    Add project files.

---
+++ WebApplication5/Controllers/StudentsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class StudentsController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        private readonly WebAppContext _context;
+
+        public StudentsController(WebAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Student.ToListAsync());
+        }
+
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,LastName,FirstName")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+    }
+}
---
+++ WebApplication5/Data/WebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+   
+    public class WebAppContext : DbContext
+    {
+        public WebAppContext(DbContextOptions<WebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp.Models.Student> Student { get; set; }
+    }
+}
